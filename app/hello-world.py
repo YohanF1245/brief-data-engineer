72d7763b73b5d200cca4e5712a738e55a693d5d0
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-07-02 22:12:22 +0200

    feat(hello_world.py): add sales table creation

--- app/hello-world.py
+++ app/hello-world.py
@@ -25,3 +25,18 @@ cursor.execute(
     )
     """
 )
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS sales (
+        product_id TEXT,
+        shop_id INTEGER,
+        created_at BIGINT NOT NULL,
+        quantity_sold INTEGER,
+        PRIMARY KEY(product_id, shop_id),
+        FOREIGN KEY(product_id) REFERENCES Products(product_id),
+        FOREIGN KEY(shop_id) REFERENCES Shops(shop_id)
+
+    )
+    """
+)
